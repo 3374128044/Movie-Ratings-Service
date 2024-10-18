3b82214f0680c979e0d70a0688cd8fa48a7fe8c3
3374128044 <alisagao27@gmail.com>
2024-10-17 22:06:25 -0700

    update requirements

---
+++ movies.py
@@ -0,0 +1,7 @@
+# add new movies
+# submit ratings for movie
+# retrieve a list of user ratings
+# fetch details for specific movie
+# update their own movie ratings
+# for admin to delete user rating from database
+# for users to delete their own ratings
