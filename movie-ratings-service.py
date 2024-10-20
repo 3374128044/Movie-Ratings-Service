72aec657c62de1ea2743286cc8baa3a998b10b4a
Sammy <96604660+itsmasso@users.noreply.github.com>
2024-10-20 11:51:57 -0700

    Update movie-ratings-service.py

--- movie-ratings-service.py
+++ movie-ratings-service.py
@@ -16,7 +16,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
     role = db.Column(db.String(50), default='user')
 
 class Movie(db.Model):
